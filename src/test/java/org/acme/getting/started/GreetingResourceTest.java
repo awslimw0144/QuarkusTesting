2aaa01ac243d9ce930922249a348694f90052598
VicLim <awslimw0144@gmail.com>
2020-04-06 00:08:45 +0800

    Second EndPoint Testing : Test for dynamic endpoint [Need to handle maven-failsafe-plugin issue]

--- src/test/java/org/acme/getting/started/GreetingResourceTest.java
+++ src/test/java/org/acme/getting/started/GreetingResourceTest.java
@@ -3,6 +3,8 @@ package org.acme.getting.started;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -10,12 +12,23 @@ import static org.hamcrest.CoreMatchers.is;
 public class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testIndexEndpoint() {
         given()
-          .when().get("/index")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/index")
+                .then()
+                .statusCode(200)
+                .body(is("Hello. This is the main page. Send keys over to this page to go to greeting page"));
     }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String strUUID = UUID.randomUUID().toString();
+        given().pathParam("name",strUUID)
+                .when().get("/index/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("Hello " + strUUID));
+    }
+
 
 }
